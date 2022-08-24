5be7bf3c32c8c87427719438f823005fd6ecc266
Omar Zeb <omar_zeb@hotmail.com>
2022-08-24 23:39:35 +0500

    validate huroku

--- val_huroku.py
+++ val_huroku.py
@@ -1,18 +1,17 @@
 import requests
 
-data = {
-            'age': 19,
-            'workclass': 'Private',
-            'fnlgt': 149184,
-            'education': 'HS-grad',
-            'marital_status': 'Never-married',
-            'occupation': 'Prof-specialty',
-            'relationship': 'Not-in-family',
-            'race': 'White',
-            'sex': 'Male',
-            'hoursPerWeek': 60,
-            'nativeCountry': 'United-States'
-    }
+data = {'age': 19,
+                 'workclass': 'Private',
+                 'fnlgt': 77516,
+                 'education': 'HS-grad',
+                 'marital-status': 'Never-married',
+                 'occupation': 'Own-child',
+                 'relationship': 'Husband',
+                 'race': 'Black',
+                 'sex': 'Male',
+                 'hours-per-week': 40,
+                 'native-country': 'United-States'
+                 }
 
 r = requests.post('https://mldevopt3.herokuapp.com/', json=data)
 print(r)
